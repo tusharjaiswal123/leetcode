7582e80ac16135ee3349cb27fa1478d0e91d56a3
Tushar Jaiswal <54629298+tusharjaiswal123@users.noreply.github.com>
2020-06-26 13:55:52 +0530

    Create Sum Root to Leaf Numbers Solution.cpp

---
+++ Sum Root to Leaf Numbers Solution.cpp
@@ -0,0 +1,89 @@
+PROBLEM:
+
+
+Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
+An example is the root-to-leaf path 1->2->3 which represents the number 123.
+Find the total sum of all root-to-leaf numbers.
+
+Note: A leaf is a node with no children.
+
+Example:
+Input: [1,2,3]
+    1
+   / \
+  2   3
+  
+Output: 25
+Explanation:
+The root-to-leaf path 1->2 represents the number 12.
+The root-to-leaf path 1->3 represents the number 13.
+Therefore, sum = 12 + 13 = 25.
+
+Example 2:
+Input: [4,9,0,5,1]
+    4
+   / \
+  9   0
+ / \
+5   1
+
+Output: 1026
+Explanation:
+The root-to-leaf path 4->9->5 represents the number 495.
+The root-to-leaf path 4->9->1 represents the number 491.
+The root-to-leaf path 4->0 represents the number 40.
+Therefore, sum = 495 + 491 + 40 = 1026.
+
+
+
+
+SOLUTION:
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void inorder(TreeNode *root,int& s)
+    {
+        if(root==NULL)
+            return;
+        
+        if(root->left!=NULL)
+        {   
+            root->left->val+=(root->val)*10;
+            inorder(root->left,s);
+        }
+        
+        if(root->right!=NULL)
+        {   
+            root->right->val+=(root->val)*10;
+            inorder(root->right,s);
+        }
+        
+        if(root->left==NULL && root->right==NULL)
+            s+=root->val;
+    }
+    
+    int sumNumbers(TreeNode* root) {
+        
+        if(root==NULL)
+            return 0;
+        
+        int s=0;
+        
+        inorder(root,s);
+       
+        return s;
+    }  
+};
